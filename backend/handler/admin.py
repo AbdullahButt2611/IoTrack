e869099945d000721a486070ab201c1ef693657f
M. Abdullah Butt <abutt2210@gmail.com>
2025-10-09 02:19:39 +0400

    refactor: clean up admin.py by removing commented code and organizing imports

--- backend/handler/admin.py
+++ backend/handler/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Device, Telemetry, Alert
 
-# Register your models here.
+admin.site.register(Device)
+admin.site.register(Telemetry)
+admin.site.register(Alert)
