188ba0b35e42f5f0964d0afc0167b9c9250c7b88
M. Abdullah Butt <abutt2210@gmail.com>
2025-10-09 01:08:42 +0400

    Initialize MQTT client in the HandlerConfig ready method

--- backend/handler/apps.py
+++ backend/handler/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
-
+import threading
 
 class HandlerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'handler'
+
+    def ready(self):
+        from . import mqtt_client
+
+        def run_mqtt():
+            mqtt_client.start_mqtt()
+
+        threading.Thread(target=run_mqtt, daemon=True).start()
